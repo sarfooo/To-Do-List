5afe6c86a88c4808971664e6c6b31367b729a5c8
Michael Sarfo <michael@Michaels-MacBook-Pro.local>
2026-03-11 19:19:13 -0400

    Finished Project

--- scripts/script.js
+++ scripts/script.js
@@ -3,19 +3,30 @@
 import ToDoListManager from "./ToDoListManager.js";
 const todoListManager = new ToDoListManager();
 
-function addTask(task) {
+function addTask(task, isDisplay) {
     let tasks = document.getElementById("task-list");
     let li = document.createElement("li");
     let marker = document.createElement("img");
+    let span = document.createElement("span");
 
     marker.src = "./images/x-mark-10349.svg";
     marker.id = "remove-task";
+    marker.addEventListener("click", () => {
+        li.remove();
+        todoListManager.removeTask(task);
+    });
     li.append(marker);
     
-    li.append(document.createTextNode(task));
-    tasks.append(li);    
-    todoListManager.addTask(task);
+    span.textContent = task;
+    li.append(span);
+    tasks.append(li); 
+    
+    if (!isDisplay) {
+        todoListManager.addTask(task);
+    }
 }
+
+
 
 function removeTask(task) {
     let tasks = document.getElementById("task-list");
@@ -27,19 +38,19 @@ function findTasks(task) {
     clearTasks()
     let found = todoListManager.findTasks(task);
     for (let task of found) {
-        addTask(task);
+        addTask(task, true);
     }
 }
 
 function clearTasks() {
     let tasks = document.getElementById("task-list")
-    tasks.innerHTML = ''
+    tasks.replaceChildren();
 }
 
 function restoreTasks() {
     clearTasks()
-    for (task of todoListManager.getTasks()) {
-        addTask(task);
+    for (let task of todoListManager.getTasks()) {
+        addTask(task, true);
     }
 }
 
@@ -49,7 +60,15 @@ taskForm.addEventListener("submit", () => {
 
     const formData = new FormData(taskForm);
     let task = formData.get("task");
-    addTask(task);
+    addTask(task, false);
+})
+
+let search = document.getElementById("search");
+search.addEventListener("input", () => {
+    if (event.target.value.length < 1) {
+        restoreTasks();
+    }
+    findTasks(event.target.value);
 })
 
 let button = document.getElementById("clear-tasks");
